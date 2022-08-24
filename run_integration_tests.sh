50370bb434f027d2bc58892697f3ca0fd8c4264a
kev-pinto-cts <kev.pinto@cts.co>
2022-08-24 14:16:36 +0100

    Testing Integration

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-#BRANCH=$1
-#COMMIT_SHA=$2
+BRANCH=$1
+COMMIT_SHA=$2
 
 echo "BRANCH=${BRANCH} COMMIT_SHA=${COMMIT_SHA}"
 
 
-#project_to_branch_map=($(cat env_mapper.txt))
-#for mapping in ${project_to_branch_map[@]}; do
-#  project_id=$(echo ${mapping}|cut -d":" -f1)
-#  dag_bucket=$(echo ${mapping}|cut -d":" -f2)
-#  branch=$(echo ${mapping}|cut -d":" -f3)
-#  echo "Project_id=${project_id}, Branch=${branch}"
-#
-#  if [ ${BRANCH} == ${branch} ]; then
-#    echo "Deploying to ${project_id}"
-#  fi
-#done
+project_to_branch_map=($(cat env_mapper.txt))
+for mapping in ${project_to_branch_map[@]}; do
+  project_id=$(echo ${mapping}|cut -d":" -f1)
+  dag_bucket=$(echo ${mapping}|cut -d":" -f2)
+  branch=$(echo ${mapping}|cut -d":" -f3)
+  echo "Project_id=${project_id}, Branch=${branch}"
+
+  if [ ${BRANCH} == ${branch} ]; then
+    echo "Deploying to ${project_id}"
+  fi
+done
