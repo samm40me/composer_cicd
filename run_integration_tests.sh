566e11cb8c31c9038c2fb636cd21d092bcc55bea
kev-pinto-cts <kev.pinto@cts.co>
2022-08-24 15:18:35 +0100

    Testing Integration

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -2,6 +2,7 @@
 
 BRANCH=$1
 SHORT_SHA=$2
+LOCATION=$3
 
 echo "BRANCH=${BRANCH} COMMIT_SHA=${COMMIT_SHA}"
 
@@ -14,7 +15,9 @@ for mapping in ${project_to_branch_map[@]}; do
 
   if [ ${BRANCH} == ${branch} ]; then
     echo "Deploying to ${project_id}"
+    gcloud auth activate-service-account sa-cicd@${PROJECT_ID}.iam.gserviceaccount.com --key-file=
     gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
+    gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/test
   fi
 
   echo "Status=${status}"
