e652440c76c5abd917e8e3a090722accdb9ed818
kev-pinto-cts <kev.pinto@cts.co>
2022-08-24 16:46:28 +0100

    Testing Integration

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -15,6 +15,7 @@ for mapping in ${project_to_branch_map[@]}; do
 
   if [ ${BRANCH} == ${branch} ]; then
     echo "Deploying to ${project_id}"
+    gcloud config set project ${project_id}
     gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
     gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
   fi
