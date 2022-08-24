b4509237a86752488354844b5f59f6a685f9e57c
kev-pinto-cts <kev.pinto@cts.co>
2022-08-24 14:33:23 +0100

    Testing Integration

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BRANCH=$1
-COMMIT_SHA=$2
+SHORT_SHA=$2
 
 echo "BRANCH=${BRANCH} COMMIT_SHA=${COMMIT_SHA}"
 
@@ -10,10 +10,10 @@ for mapping in ${project_to_branch_map[@]}; do
   project_id=$(echo ${mapping}|cut -d":" -f1)
   dag_bucket=$(echo ${mapping}|cut -d":" -f2)
   branch=$(echo ${mapping}|cut -d":" -f3)
-  echo "Project_id=${project_id}, Branch=${branch}"
+#  echo "Project_id=${project_id}, Branch=${branch}"
 
   if [ ${BRANCH} == ${branch} ]; then
     echo "Deploying to ${project_id}"
-
+    gsutil cp -r -d dags gs://{dag_bucket}/data/${SHORT_SHA}/
   fi
 done
