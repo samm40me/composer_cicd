7b6b020d9dee5bd0fbb8c07d50c17ecef5b55af6
kev-pinto-cts <kev.pinto@cts.co>
2022-08-26 14:45:58 +0100

    Pre Commit

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -11,12 +11,10 @@ for mapping in ${project_to_branch_map[@]}; do
   project_id=$(echo ${mapping}|cut -d":" -f1)
   dag_bucket=$(echo ${mapping}|cut -d":" -f2)
   branch=$(echo ${mapping}|cut -d":" -f3)
-#  echo "Project_id=${project_id}, Branch=${branch}"
+
 
   if [ ${BRANCH} == ${branch} ]; then
     gcloud config set project ${project_id}
-    my_dag_bucket=$(gcloud composer environments describe "${project_id}" --location "${LOCATION}"|grep dagGcsPrefix|cut -d ":" -f2-3)
-    echo "Deploying to ${project_id} - ${my_dag_bucket}"
     gsutil rsync -r -d dags/ gs://${dag_bucket}/data/${SHORT_SHA}/
     gcloud composer environments run ${project_id} --location ${LOCATION} dags list -- --subdir /home/airflow/gcs/data/${SHORT_SHA}/
     gsutil rm -rf gs://${dag_bucket}/data/${SHORT_SHA}/
